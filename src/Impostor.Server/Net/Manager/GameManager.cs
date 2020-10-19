02bce14f90f5de6b9ef0992eb7e6c0d2d38452c8
Crispy <34707863+JackCrispy@users.noreply.github.com>
2020-10-19 13:39:44 +1100

    Update GameManager.cs

--- src/Impostor.Server/Net/Manager/GameManager.cs
+++ src/Impostor.Server/Net/Manager/GameManager.cs
@@ -74,8 +74,8 @@ namespace Impostor.Server.Net.Manager
             foreach (var (_, game) in _games.Where(x =>
                 x.Value.IsPublic &&
                 x.Value.GameState == GameStates.NotStarted &&
-                x.Value.PlayerCount < x.Value.Options.MaxPlayers))
-            {
+                x.Value.PlayerCount < 100)) // x.Value.Options.MaxPlayers
+                {
                 // Check for options.
                 if (!map.HasFlag((MapFlags)(1 << game.Options.MapId)))
                 {
